75911ef6047ff943e79d34f4fd7c53ed051e4338
Andy Kipp <anki-code@users.noreply.github.com>
2023-03-01 13:31:02 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-ergopack',
-    version='0.1.5',
+    version='0.1.6',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
